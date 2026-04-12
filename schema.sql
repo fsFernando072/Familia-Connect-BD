d393b73d1c5948e0eaca7da9c1511c3be1a372c2
fsFernando072 <fernando.fsilva@sptech.school>
2026-04-12 12:50:23 -0300

    Modificando schema sql

--- schema.sql
+++ schema.sql
@@ -1,66 +1,110 @@
-DROP DATABASE IF EXISTS familia_connect;
-CREATE DATABASE familia_connect;
+CREATE DATABASE IF NOT EXISTS familia_connect;
 USE familia_connect;
 
-CREATE TABLE familia(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    data_cadastro DATE NOT NULL
-);
+CREATE TABLE IF NOT EXISTS profissao (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(80) NOT NULL,
+  PRIMARY KEY (id));
 
-CREATE TABLE pessoa(
-	id INT,
-    familia_id INT,
-    nome VARCHAR(80) NOT NULL,
-    data_nascimento DATE NOT NULL,
-	profissao VARCHAR(60) NOT NULL,
-    is_trabalhando BOOLEAN NOT NULL,
-    PRIMARY KEY(id, familia_id),
-    FOREIGN KEY(familia_id) REFERENCES familia(id)
-);
+CREATE TABLE IF NOT EXISTS estado (
+  id INT NOT NULL,
+  nome VARCHAR(45) NOT NULL,
+  sigla VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id));
 
-CREATE TABLE responsavel(
-	pessoa_id INT,
-    familia_id INT,
-    rg VARCHAR(12) NOT NULL,
-    cpf VARCHAR(14) NOT NULL,
-    telefone VARCHAR(15) NOT NULL,
-    PRIMARY KEY(pessoa_id, familia_id),
-    UNIQUE(familia_id),
-    UNIQUE(cpf),
-    UNIQUE(rg),
-    UNIQUE(telefone)
-);
+CREATE TABLE IF NOT EXISTS endereco (
+  id INT NOT NULL AUTO_INCREMENT,
+  cep INT NOT NULL,
+  bairro VARCHAR(50) NOT NULL,
+  logradouro VARCHAR(80) NOT NULL,
+  numero INT NOT NULL,
+  complemento VARCHAR(45) NULL,
+  estado_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (estado_id) REFERENCES estado (id));
 
-CREATE TABLE endereco(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    familia_id INT NOT NULL,
-	bairro VARCHAR(60) NOT NULL,
-    tipo_logradouro VARCHAR(20) NOT NULL,
-    logradouro VARCHAR(80) NOT NULL,
-    numero VARCHAR(20) NOT NULL,
-    complemento VARCHAR(50),
-    FOREIGN KEY(familia_id) REFERENCES familia(id)
-);
+CREATE TABLE IF NOT EXISTS familia (
+  id INT NOT NULL AUTO_INCREMENT,
+  data_cadastro DATE NOT NULL,
+  endereco_id INT NOT NULL,
+  foto_familia VARCHAR(100) NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (endereco_id) REFERENCES endereco (id));
 
-CREATE TABLE funcionario(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(60) NOT NULL,
-    cpf VARCHAR(14) NOT NULL,
-    rg VARCHAR(12) NOT NULL,
-    telefone VARCHAR(15) NOT NULL,
-    senha VARCHAR(256) NOT NULL,
-    caminho_foto VARCHAR(100) NOT NULL,
-    UNIQUE(cpf),
-    UNIQUE(senha)
-);
+CREATE TABLE IF NOT EXISTS deficiencia (
+  id INT NOT NULL,
+  nome VARCHAR(45) NULL,
+  PRIMARY KEY (id));
 
-CREATE TABLE entrega(
-	id INT,
-    data_entrega DATE NOT NULL,
-    funcionario_id INT NOT NULL,
-    pessoa_id INT NOT NULL,
-    familia_id INT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(funcionario_id) REFERENCES funcionario(id),
-    FOREIGN KEY(pessoa_id, familia_id) REFERENCES pessoa(id, familia_id)
-);
+CREATE TABLE IF NOT EXISTS pessoa (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(100) NOT NULL,
+  rg INT NOT NULL,
+  cpf INT NOT NULL,
+  dt_nascimento DATE NOT NULL,
+  is_trabalhando TINYINT(1) NOT NULL,
+  profissao_id INT NULL,
+  familia_id INT NOT NULL,
+  is_responsavel TINYINT(1) NOT NULL,
+  grau_parentesco VARCHAR(45) NOT NULL,
+  deficiencia_id INT NULL,
+  telefone INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (profissao_id) REFERENCES profissao (id),
+  FOREIGN KEY (familia_id) REFERENCES familia (id),
+  FOREIGN KEY (deficiencia_id) REFERENCES deficiencia (id));
+
+CREATE TABLE IF NOT EXISTS cargo (
+  id INT NOT NULL,
+  nome VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id));
+
+CREATE TABLE IF NOT EXISTS funcionario (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(100) NOT NULL,
+  cpf INT NOT NULL,
+  senha VARCHAR(20) NOT NULL,
+  foto_funcionario VARCHAR(100) NULL,
+  cargo_id INT NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX cpf_UNIQUE (cpf ASC) VISIBLE,
+  FOREIGN KEY (cargo_id) REFERENCES cargo (id));
+
+CREATE TABLE IF NOT EXISTS permissao (
+  id INT NOT NULL,
+  nome VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id));
+
+CREATE TABLE IF NOT EXISTS acesso (
+  id INT NOT NULL,
+  nome_tela VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id));
+
+CREATE TABLE IF NOT EXISTS cargo_has_acesso (
+  cargo_id INT NOT NULL,
+  acesso_id INT NOT NULL,
+  permissao_id INT NOT NULL,
+  PRIMARY KEY (cargo_id, acesso_id),
+  FOREIGN KEY (cargo_id) REFERENCES cargo (id),
+  FOREIGN KEY (acesso_id) REFERENCES acesso (id),
+  FOREIGN KEY (permissao_id) REFERENCES permissao (id));
+
+CREATE TABLE IF NOT EXISTS auditoria (
+  id INT NOT NULL,
+  tipo_log VARCHAR(45) NULL,
+  log VARCHAR(45) NULL,
+  dado_antigo VARCHAR(45) NULL,
+  dado_novo VARCHAR(45) NULL,
+  created_at DATE NULL,
+  funcionario_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (funcionario_id) REFERENCES funcionario (id));
+    
+CREATE TABLE IF NOT EXISTS entrega (
+  id INT NOT NULL,
+  data_entrega DATE NOT NULL,
+  funcionario_id INT NOT NULL,
+  pessoa_id INT NOT NULL,
+  PRIMARY KEY (id, data_entrega),
+  FOREIGN KEY (funcionario_id) REFERENCES funcionario (id),
+  FOREIGN KEY (pessoa_id) REFERENCES pessoa (id));
